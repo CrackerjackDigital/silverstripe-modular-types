7327ef0f194fa8ad2632d5e610ba063b6d6e30ee
Steve Wake <steve@moveforward.co.nz>
2017-02-22 00:03:43 +1300

    moved classes into here from modular module

---
+++ code/extensions/Type.php
@@ -0,0 +1,12 @@
+<?php
+namespace Modular\Extensions\Model;
+use Modular\ModelExtension;
+
+/**
+ * Add to a model to make it 'Typeish'
+ *
+ * @package Modular
+ */
+class Type extends ModelExtension  {
+
+}
